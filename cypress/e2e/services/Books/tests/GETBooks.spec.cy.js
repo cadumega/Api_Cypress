1ef72657f19349aa2c499f45d81aea810543f8f0
cadumega <cadumega@outlook.com>
2022-09-02 00:55:44 -0300

    fix: archives tests

--- cypress/e2e/services/Books/tests/GETBooks.spec.cy.js
+++ cypress/e2e/services/Books/tests/GETBooks.spec.cy.js
@@ -1,10 +1,20 @@
-import * as GETBooks from '../requests/GETBooks.request.cy';
+import * as GETBooks from '../requests/GETBooks.request';
 
 describe ('GET Books', () => {
     it('Listar todos os livros', ()=> {
         GETBooks.allBooks().should((response) => {
             cy.log(response.status)
             cy.log(response.statusText)
+            cy.log(response.body)
+
+            expect(response.status).to.eq(200);
+            expect(response.body).to.be.not.null
         })
     })
 })
+
+
+
+// Só validamos se o response veio 200
+// Iremos fazer + um expect, o retorno não pode ser nulo, como uma array vazia q poderia vir.
+// Primeiro test concluído para utilizarmos eles.
